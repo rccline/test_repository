ca58d264429cca63ee69bb7f844d199ef6821490
Robert Cline <rccline@fastmail.fm>
2022-04-18 09:57:45 -0600

    Modified new-file-2.R on the features branch.

--- new-file-2.R
+++ new-file-2.R
@@ -1,1 +1,13 @@
-This a new content file.   after f341889  
+# This a new content file.   after f341889  
+
+# remote: Resolving deltas: 100% (1/1), completed with 1 local object.
+# To https://github.com/rccline/test_repository.git
+# 39f545b..db6d6e8  master -> master 
+
+
+# Robert@Rclin141132 MINGW64 /e/test_repository (features/new-file2)
+# $ git status
+# On branch features/new-file2
+# nothing to commit, working tree clean 
+
+## Now, I commit these changes.  
